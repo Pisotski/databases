a5e1249588018f0a563f5717f35aa592b1f9b3c5
Pisotski <vpisotski@gmail.com>
2018-05-18 20:23:26 -0700

    completed schemas

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,11 @@
 var db = require('../db');
+var express = require('express');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function (req, res) {
+      
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,39 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  userID INT(30) AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE rooms (
+  roomID INT(30) AUTO_INCREMENT PRIMARY KEY,
+  roomname VARCHAR(15) NOT NULL
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  messageID INT(30) AUTO_INCREMENT PRIMARY KEY,
+  message VARCHAR(300) NOT NULL,
+  /* will be reference to external USER ID*/
+  userID INT(30) REFERENCES users(userID),
+  roomID INT(30) REFERENCES rooms(roomID)
+  -- FOREIGN KEY (userID) REFERENCES users(userID),
+  -- FOREIGN KEY (roomID) REFERENCES rooms(roomID)
 );
 
 /* Create other tables and define schemas for them here! */
-
-
+-- CREATE TABLE friends (
+--   friendsID INT(30) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+--   userID INT(30),
+--   userfriendID INT(30),
+--   FOREIGN KEY (userID) REFERENCES users(userID),
+--   FOREIGN KEY (userfriendID) REFERENCES users(userfriendID)
+-- );
+/* 
+ALTER TABLE --> 
+  look into ADD 
+*/
 
 
 /*  Execute this file from the command line by typing:
